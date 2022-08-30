5482ee8bfc69c430d76847189be7c93591bb2964
Phil Lin <ts-pinhung.lin@rakuten.com>
2022-08-30 17:28:28 +0900

    tmp

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,9 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  buildAssetsDir: '/testing/_nuxt/'
+  runtimeConfig: {
+    public: {
+       baseURL: '/testing/' // Exposed to the frontend as well.
+    }
+  }
 })
