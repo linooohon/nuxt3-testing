0c2022b465f79b6829962e6a5d7faba689edba98
Phil Lin <ts-pinhung.lin@rakuten.com>
2022-08-31 18:37:55 +0900

    tmpppp

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,15 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  // router: {
+  //   base: '/antares_normalization'
+  // },
   runtimeConfig: {
     app: {
        baseURL: '/testing/',
-      //  buildAssetsDir: '/buildtest/' // Exposed to the frontend as well.
+       cdnURL: '/testing/'
+  //      buildAssetsDir: '/_nuxt/' // Exposed to the frontend as well.
     },
-    ssr: false
+  //   // ssr: false
   }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,12 @@
 <template>
     index.vue
+    {{ hello }}
 </template>
+
+
+
+
+<script setup>
+const config = useRuntimeConfig()
+const hello = await $fetch(config.app.baseURL + 'api/hello')
+</script>
