d94e217059a4ee8ea4de2fbafb7a47e4f4dc70b6
Phil Lin <ts-pinhung.lin@rakuten.com>
2022-08-31 20:52:11 +0900

    adddd

--- env.js
+++ env.js
@@ -1,25 +1,52 @@
 // NODE_ENV: 'development'
 const DEV = {
-    BASE_URL: '/root/',
-    CDN_URL: '/',
+  BASE_URL: '/root/',
+  CDN_URL: '/',
+}
+
+// NODE_ENV: 'production'
+const STG = {
+  BASE_URL: '/root/',
+  CDN_URL: '/',
+}
+
+// NODE_ENV: 'production'
+// In netlify CDN_URL should be root: '/'
+const PRO = {
+  BASE_URL: '/root/',
+  CDN_URL: '/',
+}
+
+// but in normal use case: when we set the root 'BASE_URL' to ex: '/root/'
+// CDN_URL should also be '/root/'
+/*
+
+// NODE_ENV: 'development'
+const DEV = {
+  BASE_URL: '/root/',
+  CDN_URL: '/',
+}
+ 
+// NODE_ENV: 'production'
+const STG = {
+  BASE_URL: '/root/',
+  CDN_URL: '/root/',
+}
+ 
+// NODE_ENV: 'production'
+// In netlify CDN_URL should be root: '/'
+const PRO = {
+  BASE_URL: '/root/',
+  CDN_URL: '/root/',
+}
+
+*/
+
+
+module.exports = {
+  envConfig: {
+    'development': DEV,
+    'staging': STG,
+    'production': PRO
   }
-  
-  // NODE_ENV: 'production'
-  const STG = {
-    BASE_URL: '/root/',
-    CDN_URL: '/root/',
-  }
-  
-  // NODE_ENV: 'production'
-  const PRO = {
-    BASE_URL: '/root/',
-    CDN_URL: '/',
-  }
-  
-  module.exports = {
-    envConfig: {
-      'development': DEV,
-      'staging': STG,
-      'production': PRO
-    }
-  }
+}
